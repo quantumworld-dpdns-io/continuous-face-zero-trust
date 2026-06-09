6d28431d9229d13781ec9db208e1744b3e151738
Dennislee:) <55692615+dennislee928@users.noreply.github.com>
2026-06-09 12:12:02 +0800

    Auto-commit: Project implementation in progress [count: 727]

---
+++ infra/terraform/providers.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_version = ">= 1.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.27"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "google" {
+  project = var.gcp_project_id
+  region  = var.gcp_region
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
